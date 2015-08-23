9e8d8b62e960b9e8110d4052ca819b51717f489d
bimofan <youlikethisgame@gmail.com>
2015-08-23 17:12:51 +0800

    zhu

--- Taling/Taling/BaseViewControllers/BaseTableViewController.m
+++ Taling/Taling/BaseViewControllers/BaseTableViewController.m
@@ -19,6 +19,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //去掉底部黑线
+    if ([self.navigationController.navigationBar respondsToSelector:@selector( setBackgroundImage:forBarMetrics:)]){
+        NSArray *list=self.navigationController.navigationBar.subviews;
+        for (id obj in list) {
+            if ([obj isKindOfClass:[UIImageView class]]) {
+                UIImageView *imageView=(UIImageView *)obj;
+                NSArray *list2=imageView.subviews;
+                for (id obj2 in list2) {
+                    if ([obj2 isKindOfClass:[UIImageView class]]) {
+                        UIImageView *imageView2=(UIImageView *)obj2;
+                        imageView2.hidden=YES;
+                    }
+                }
+            }
+        }
+    }
+    
      self.view.backgroundColor = kBackgroundColor;
     
 //    self.tabBarController.tabBar.tintColor = TabbarTintColor;
